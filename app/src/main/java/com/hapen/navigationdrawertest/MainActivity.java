d55031a5740e699a88001109199a1673f9c1c248
Phonpaseuth Khamsenesouk <phonpaseuth@gmail.com>
2019-04-17 16:21:50 -0500

    merge Nneka's code to master branch

--- app/src/main/java/com/hapen/navigationdrawertest/MainActivity.java
+++ app/src/main/java/com/hapen/navigationdrawertest/MainActivity.java
@@ -1,23 +1,11 @@
 package com.hapen.navigationdrawertest;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.MenuInflater;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -26,39 +14,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.Toast;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.LinearLayout;
-
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
-import java.util.HashMap;
-
-//public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, SearchView.OnQueryTextListener {
-import com.dmallcott.dismissibleimageview.DismissibleImageView;
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
-<<<<<<< HEAD
-=======
 import com.google.firebase.auth.FirebaseUser;
->>>>>>> master
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-<<<<<<< HEAD
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-import android.content.SharedPreferences;
-
-
-=======
-import com.google.firebase.database.ValueEventListener;
->>>>>>> master
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -69,7 +33,6 @@ public class MainActivity extends AppCompatActivity
     private RecyclerView mRecyclerView;
     DatabaseReference ref;
 
-
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -78,15 +41,12 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//Actionbar
+        //Actionbar
         ActionBar actionBar = getSupportActionBar();
         //set title
-
-
         //Action Bar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -123,7 +83,6 @@ public class MainActivity extends AppCompatActivity
         });
     }
 
-<<<<<<< HEAD
     @Override
     protected void onStart() {
         super.onStart();
@@ -131,8 +90,6 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-=======
->>>>>>> master
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -240,9 +197,6 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
-
-
 }
 
 
